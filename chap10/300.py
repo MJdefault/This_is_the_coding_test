082066a6faf50fc27e0d930ed6dd308a8ea11f7d
MJdefault <110967649+MJdefault@users.noreply.github.com>
2023-04-28 22:31:36 +0900

    Create 300.py

---
+++ chap10/300.py
@@ -0,0 +1,27 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+
+for i in range(0, n+1):
+    parent[i] = i
+
+for i in range(m):
+    operate, a, b = map(int, input().split())
+    if operate == 0:
+        union_parent(parent, a, b)
+    elif operate == 1:
+        if find_parent(parent, a) == find_parent(parent, b):
+            print('YES')
+        else:
+            print('NO')
